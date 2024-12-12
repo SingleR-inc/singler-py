be176facc71cd00a1cf2438b6677f2c31144ab56
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-12-11 21:11:31 -0800

    Got all the C++ code compiling again.

--- lib/src/init.cpp
+++ lib/src/init.cpp
@@ -4,11 +4,15 @@
 void init_find_classic_markers(pybind11::module&);
 void init_train_single(pybind11::module&);
 void init_classify_single(pybind11::module&);
+void init_train_integrated(pybind11::module&);
+void init_classify_integrated(pybind11::module&);
 
 PYBIND11_MODULE(lib_singler, m) {
     init_find_classic_markers(m);
     init_train_single(m);
     init_classify_single(m);
+    init_train_integrated(m);
+    init_classify_integrated(m);
 
     pybind11::class_<TrainedSingleIntersect, TrainedSingleIntersectPointer>(m, "TrainSingleIntersect");
     pybind11::class_<TrainedIntegrated, TrainedIntegratedPointer>(m, "TrainIntegrated");
